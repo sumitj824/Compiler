2eaf38b0efb5272fbd91065f7674e5a4ae468fc9
nivedh1 <nivedh@iitk.ac.in>
2021-02-06 18:17:26 +0530

    test cases added:

---
+++ tests/test3.c
@@ -0,0 +1,22 @@
+//testing data types
+
+typedef unsigned char BYTE;
+
+struct point{
+    int x;
+    int y;
+};
+
+static int num = 6;
+int k = 9;
+
+int main(){
+    int a = 5;
+    float b = 5.0000;
+    char c = 'c';
+    double d = 7.0000000;
+    unsigned int e = 11;
+    short f = 12;
+    long g = 133333;
+    int arr[5] = {1,2,3,4,5};
+}
---
+++ tests/test4.c
@@ -0,0 +1,20 @@
+//for conditionals
+
+int main(){
+    //if condition
+    int a = 5+6+7;
+    int b = 3+9+6;
+    if( a == b){
+        printf("yes!!\n");
+    }else{
+        printf("NO!!\n");
+    }
+
+    //? conditional operator
+
+    int c = (a>b)?a:b;
+
+    //switch statement
+
+    switch()
+}
