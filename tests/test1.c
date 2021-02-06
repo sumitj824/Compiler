b0c09bcda2f64ee5caa2ccf117427957336cf434
kartik3108 <kartiknirmal2000@gmail.com>
2021-02-06 23:53:41 +0530

    test1.c and test2.c completed

--- tests/test1.c
+++ tests/test1.c
@@ -1,12 +1,32 @@
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
 
+struct ar{
+    float x;
+    int y;
+    char *s;
+};
 /* multiple
     line */
 
 //single line comment
 
+
 int main() {
  
  /**/int x=0;
+
+ struct ar instant;
+ instant.x = 100;
+ instant.s = (char *)malloc(5*sizeof(char));
+ instant.s[0] = 'a';
+ instant.s[1] = 'b';
+ instant.s[2] = 'c';
+ instant.s[3] = 'd';
+ instant.s[4] = '\0';
+
+ printf("%d  %f  %s", x, instant.x, instant.s);
 
  return 0;
 
--- tests/test2.c
+++ tests/test2.c
@@ -1,3 +1,20 @@
+#include<stdio.h>
+
+union xy_z{
+    int  *ptr;
+    float x;
+};
+
 int main(){
-    
+    union xy_z curr;
+    curr.x = 15/5;
+    printf("%ld \n",sizeof(curr));
+    for(int i = 0; i < 5; i++){
+        if(i %2)
+        {
+            printf("%ld \n", sizeof(curr.ptr));
+        }
+        else printf ("%f \n",curr.x);
+    }
+    return 0;
 }
