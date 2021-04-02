5583ce7bc7fdbcd42176749b12f47705d0883e2a
visitor-4137 <sourabhmatrix7@gmail.com>
2021-04-02 10:52:31 +0530

    added make_symTable_entry function

--- src/symtable.cpp
+++ src/symtable.cpp
@@ -6,10 +6,16 @@ using namespace std;
 
 map<symTable*,symTable*> parent;
 symTable Global;
-symTable *cur;
+symTable *cur = new symTable();
 
 
-
+void make_symTable_entry(string name,string type,int init){
+    cout << name << " with " << type << " init : " << init << endl;  
+    s_entry * p = new s_entry();
+    p -> type = type;
+    p -> init = init;
+    (*cur).insert({name,p});
+}
 
 
 
@@ -23,4 +29,3 @@ s_entry* lookup(string a)
     }
     return NULL;
 }
-
