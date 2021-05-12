11c9eb3252492803306ee1e96ac2d5db7b38021b
h11nt3r <sumitj824@gmail.com>
2021-05-12 14:18:57 +0530

    fixed some error

--- tests/11_Matrix_Multiplication.c
+++ tests/11_Matrix_Multiplication.c
@@ -1,5 +1,5 @@
 
-void getMatrixElements(int matrix[][10], int row, int column) {
+void getMatrixElements(int matrix[][9], int row, int column) {
    int i,j;
 //    printf("\nEnter elements: \n");
    
@@ -7,24 +7,25 @@ void getMatrixElements(int matrix[][10], int row, int column) {
       for ( j = 0; j < column; ++j) {
         //  printf("Enter a%d%d: ", i + 1, j + 1);
         //  scanf("%d", &matrix[i][j]);
+        matrix[i][j] =1;
       }
    }
    return ;
 }
 
 
-void multiplyMatrices(int first[][10],int second[][10],int result[][10],int r1, int c1, int r2, int c2) {
+void multiplyMatrices(int first[][9],int second[][9],int result[][9],int r1, int c1, int r9, int c9) {
 
    int i,j,k;
    for ( i = 0; i < r1; ++i) {
-      for ( j = 0; j < c2; ++j) {
+      for ( j = 0; j < c9; ++j) {
          result[i][j] = 0;
       }
    }
 
   
    for ( i = 0; i < r1; ++i) {
-      for ( j = 0; j < c2; ++j) {
+      for ( j = 0; j < c9; ++j) {
          for ( k = 0; k < c1; ++k) {
             result[i][j] += first[i][k] * second[k][j];
          }
@@ -34,15 +35,15 @@ void multiplyMatrices(int first[][10],int second[][10],int result[][10],int r1, 
 }
 
 
-void display(int result[][10], int row, int column) {
+void display(int result[][9], int row, int column) {
    int i,j;
    // printf("\nOutput Matrix:\n");
    for ( i = 0; i < row; ++i) {
       for ( j = 0; j < column; ++j) {
-         // printf("%d  ", result[i][j]);
-         if (j == column - 1){
-            // printf("\n");
-         }
+         printf( result[i][j]);
+         // if (j == column - 1){
+         //    // printf("\n");
+         // }
 
       }
    }
@@ -50,17 +51,20 @@ void display(int result[][10], int row, int column) {
 }
 
 int main() {
-    int first[10][10], second[10][10], result[10][10], r1, c1, r2, c2;
+    int first[9][9], second[9][9], result[9][9], r1, c1, r9, c9;
     // printf("Enter rows and column for the first matrix: ");
     // scanf("%d %d", &r1, &c1);
     // printf("Enter rows and column for the second matrix: ");
-    // scanf("%d %d", &r2, &c2);
-
+    // scanf("%d %d", &r9, &c9);
+   r1=9;
+   r9=9;
+   c1=9;
+   c9=9;
 
     getMatrixElements(first, r1, c1);
-    getMatrixElements(second, r2, c2);
-    multiplyMatrices(first, second, result, r1, c1, r2, c2);
-    // display(result, r1, c2);
+    getMatrixElements(second, r9, c9);
+    multiplyMatrices(first, second, result, r1, c1, r9, c9);
+    display(result, r1, c9);
 
    return 0;
 }
